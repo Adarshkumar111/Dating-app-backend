70e2956b3a4cceee8e71aca9746d5cdb02a9aada
Adarsh Kumar <adarsh.kumar232@lpu.in>
2025-10-08 17:47:14 +0530

    fix all bug

--- config/envConfig.js
+++ config/envConfig.js
@@ -5,7 +5,7 @@ export const env = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mnikah',
   JWT_SECRET: process.env.JWT_SECRET || 'dev_secret_change_me',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5174',
   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID || '',
   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET || '',
   IMAGEKIT_PUBLIC_KEY: process.env.IMAGEKIT_PUBLIC_KEY || '',
